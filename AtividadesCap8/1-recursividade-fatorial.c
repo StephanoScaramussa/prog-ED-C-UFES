5147d6c87da256fc4491a0ee729ee44833b94519
Stephano <98617499+StephanoScaramussa@users.noreply.github.com>
2025-02-18 11:33:36 -0300

    Create 1-recursividade-fatorial.c

---
+++ AtividadesCap8/1-recursividade-fatorial.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int fatorial(int n){
+	if (n <=1 )
+		return 1;
+	else
+		return n * fatorial(n-1);
+}
+
+int main(){
+	int num, result;
+	printf("Digite um n\xa3mero: \n");
+	scanf("%d", &num);
+	
+	result = fatorial(num);
+	
+	printf("Resultado da fatorial de %d \x82: %d \n", num,result);
+}
