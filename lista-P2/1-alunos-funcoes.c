a3aeeb369e8a1de7816a134a8aa45050938538b0
Stephano <98617499+StephanoScaramussa@users.noreply.github.com>
2025-03-17 11:46:16 -0300

    Create 1-alunos-funcoes.c

---
+++ lista-P2/1-alunos-funcoes.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define qtd_alunos 10
+#define qtd_disciplinas 5
+#define max_nome 100
+
+typedef struct{
+	char nome[max_nome];
+	float media[qtd_disciplinas];
+	float nota_final[qtd_disciplinas];
+} Aluno;
+
+void registrar_Dados(Aluno turma[]){
+	
+}
+
+void alunos_Reprovados(Aluno turma[]){
+	
+}
+
+void salvar_Arquivo(Aluno turma[]){
+	
+}
+
+void ler_Arquivo(Aluno turma[]){
+	
+}
+
+main(){
+	Aluno turma[qtd_alunos];
+}
