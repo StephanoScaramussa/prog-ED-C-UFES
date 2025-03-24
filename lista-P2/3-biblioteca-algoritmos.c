933e423e0626481bde3d5b9e9ce928be004257d6
Stephano <98617499+StephanoScaramussa@users.noreply.github.com>
2025-03-24 13:55:47 -0300

    Update 3-biblioteca-algoritmos.c

--- lista-P2/3-biblioteca-algoritmos.c
+++ lista-P2/3-biblioteca-algoritmos.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 #define qtd_obras 3
-#define qtd_disc 3
+#define qtd_areas 3
 #define max_char 100
 
 typedef struct{
@@ -15,38 +15,40 @@ typedef struct{
     int paginas;
     char palavras_chaves[max_char];
     char assunto[max_char];
+    int area;
 }Obra;
 
-typedef struct{
-    char nome[max_char];
-    Obra vet_obras[qtd_obras];
-}Materia;
-
-int pesquisaCodigo(Materia *vet_materia){
+int pesquisaCodigo(Obra *vet_obras){
     int codigo;
     printf("Digite o codigo da obra: ");
     scanf("%d", &codigo);
-    for(int i=0;i<qtd_disc;i++){
-        for(int j=0;j<qtd_obras;j++){
-            if(vet_materia[i].vet_obras[j].codigo == codigo){
-                printf("Codigo encontrado na materia %s, obra %s, posição %d/%d\n", vet_materia[i].nome, vet_materia[i].vet_obras[j].nome,i,j);
-                return j;
+    for(int i=0;i<qtd_areas;i++){
+        if(vet_obras[i].codigo == codigo){
+            if(vet_obras[i].area == 0){
+                printf("Codigo encontrado da obra %s, em Ciências Exatas, posição %d\n", vet_obras[i].nome,i);
+                return i;
+            }
+            if(vet_obras[i].area == 1){
+                printf("Codigo encontrado da obra %s, em Ciências Humanas, posição %d\n", vet_obras[i].nome,i);
+                return i;
+            }
+            if(vet_obras[i].area == 2){
+                printf("Codigo encontrado da obra %s, Ciências Biomédicas, posição %d\n", vet_obras[i].nome,i);
+                return i;
             }
         }
     }
     return -1;
 }
 
-int obrasAutor(Materia *vet_materia){
+int obrasAutor(Obra *vet_obras){
     char autor_[max_char];
-    int qtd_obras_autor=0;
+    int qtd_obras_autor = 0;
     printf("Digite o nome do autor: ");
-    scanf("%[^\n]", autor_);
-    for(int i=0;i<qtd_disc;i++){
-        for(int j=0;j<qtd_obras;j++){
-            if(*autor_ == *vet_materia[i].vet_obras[j].nome){
-                qtd_obras_autor++;
-            }
+    scanf(" %[^\n]", autor_);
+    for(int i = 0; i < qtd_obras; i++){
+        if(strcmp(autor_, vet_obras[i].autor) == 0){
+            qtd_obras_autor++;
         }
     }
     printf("%s tem %d obras\n", autor_, qtd_obras_autor);
@@ -54,30 +56,11 @@ int obrasAutor(Materia *vet_materia){
 }
 
 void main(){
-    Materia vet_materia[]={
-        {
-            "Ciências Exatas",{
-                {1, "lorem", "Seymour Lipschutz", "McGraw-Hill", "Sim", 500, "Matemática, Estatística", "Matemática"},
-                {2, "ipsum", "Seymour Lipschutz", "McGraw-Hill", "Não", 1500, "Portugues, Gramatica", "Linguagens"},
-                {3, "dolor", "KhaZhan", "Fores", "Sim", 2000, "Matemática, Calculo", "Integrais"}
-            }
-        },
-            {"Ciências Humanas",{
-                {1, "lorem", "Seymour Lipschutz", "McGraw-Hill", "Sim", 500, "Matemática, Estatística", "Matemática"},
-                {2, "densin", "Seymour", "Hill", "Não", 1500, "Portugues, Gramatica", "Linguagens"},
-                {3, "opren", "KhaZhan", "Fores", "Sim", 2000, "Matemática, Calculo", "Integrais"}
-            }
-        },
-        {
-            "Ciências Biomédicas",{
-                {1, "lorem", "Seymour Lipschutz", "McGraw-Hill", "Sim", 500, "Matemática, Estatística", "Matemática"},
-                {2, "ipsum", "Seymour Lipschutz", "McGraw-Hill", "Não", 1500, "Portugues, Gramatica", "Linguagens"},
-                {3, "dolor", "KhaZhan", "Fores", "Sim", 2000, "Matemática, Calculo", "Integrais"}
-            }
-        }
+    Obra vet_obras[qtd_obras] = {
+        {1, "lorem", "Seymour Lipschutz", "McGraw-Hill", "Sim", 500, "Matemática, Estatística", "Matemática", 0},
+        {2, "ipsum", "Seymour Lipschutz", "McGraw-Hill", "Não", 1500, "Portugues, Gramatica", "Linguagens", 1},
+        {3, "dolor", "KhaZhan", "Fores", "Sim", 2000, "Matemática, Calculo", "Integrais", 0}
     };
-
-    pesquisaCodigo(vet_materia);
-    obrasAutor(vet_materia);
-
+    pesquisaCodigo(vet_obras);
+    obrasAutor(vet_obras);
 }
