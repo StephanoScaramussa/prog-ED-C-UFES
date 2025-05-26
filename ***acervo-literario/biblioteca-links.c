417a5a1b2a5c36af414f7f618e98248d68844a99
Stephano <98617499+StephanoScaramussa@users.noreply.github.com>
2025-05-26 15:51:35 -0300

    Create biblioteca-links.c

---
+++ ***acervo-literario/biblioteca-links.c
@@ -0,0 +1,4 @@
+/*ACESSO AS BIBLIOTECAS VIRTUAIS UFES:
+1.https://www.bing.com/ck/a?!&&p=32dcd7a0565b845e974cc49df48e001495403a2a44fe2c453f24e64e838ffe2aJmltdHM9MTc0ODIxNzYwMA&ptn=3&ver=2&hsh=4&fclid=0430d912-8637-6912-23f8-cc3c876668ce&psq=ufes+minha+biblioteca&u=a1aHR0cHM6Ly9iaWJsaW90ZWNhcy1kaWdpdGFpcy51ZmVzLmJyLw&ntb=1
+
+*/
