55f50ccb5ef3db1da4902690a3d65ffae5a3c5f7
Stephano <98617499+StephanoScaramussa@users.noreply.github.com>
2025-06-05 15:50:01 -0300

    Update lista-descritor-main.c

--- estrutura-de-dados1/lista-descritor-main.c
+++ estrutura-de-dados1/lista-descritor-main.c
@@ -3,8 +3,33 @@
 
 int main(){
 	Descritor lista;
-	criarLista(&lista); //envia endereço para acessar com ponteiro nas funções 'NoLista* l'
-	insereNoInicio(&lista,2);
-	insereNoInicio(&lista,5);
+	criaLista(&lista); //envia endereço para acessar com ponteiro nas funções 'NoLista* l'
+	insereNoFinal(&lista,55);
+	insereNoFinal(&lista,75);
+	insereNoInicio(&lista,95);
 	imprimeLista(&lista);
+	removeDaLista(&lista,55);
+	printf("\n");
+	imprimeLista(&lista);
+	removeDaLista(&lista,95);
+	removeDaLista(&lista,75);
+	printf("\n");
+	imprimeLista(&lista);
+	insereNoFinal(&lista,5);
+	insereNoFinal(&lista,7);
+	insereNoFinal(&lista,9);
+	printf("\n");
+	imprimeLista(&lista);
+	liberaLista(&lista);
+	printf("\n");
+	imprimeLista(&lista);
+	insereNoFinal(&lista,5);
+	insereNoFinal(&lista,7);
+	insereNoFinal(&lista,9);
+	printf("\n");
+	imprimeLista(&lista);
+	NoLista* p = buscaNaLista(&lista,7);
+	if(p != NULL){
+		printf("\n%d",p->info);
+	}
 }
