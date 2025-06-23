e41bd211458100482283553ab9baaa9937fe7ad7
Stephano <98617499+StephanoScaramussa@users.noreply.github.com>
2025-06-23 03:09:55 -0300

    Update registro-de-humor.c

--- estrutura-de-dados1/trabalho-1/registro-de-humor.c
+++ estrutura-de-dados1/trabalho-1/registro-de-humor.c
@@ -1,14 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "registro-de-humor.h"
 
-typedef struct registrodehumor{
-    int id;
-    char data[10];
-    int humor;
-    char motivo[100];
-    int notaDoDia;
-}RegistroDeHumor;
-
-RegistroDeHumor*criarRegistro(int i_id, char i_data[10], int i_humor, char i_motivo[100], int i_nota);
-void imprimirRegistro(int i_id);
-void liberarRegistro(int i_id);
+RegistroDeHumor*criarRegistro(int i_id, char i_data[10], int i_humor, char i_motivo[100], int i_nota){
+    RegistroDeHumor*novo=malloc(sizeof(RegistroDeHumor));
+    novo->id = i_id;
+    novo->data[10] = i_data;
+    novo->humor = i_humor;
+    novo->motivo[100] = i_motivo;
+    novo->notaDoDia = i_nota;
+}
+void imprimirRegistro(int i_id){
+    
+}
