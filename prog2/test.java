603b610f367b1ad4e51978c414cab2648b0e65a6
Stephano Dorigo Scaramussa <stephano.scaramussa>
2025-10-23 16:34:35 -0300

    mudancas

--- prog2/test.java
+++ prog2/test.java
@@ -1,6 +1,6 @@
 //nao pode haver outra classe externa publica
 //classe externa nunca pode ser privada
-public class ExampleForTest{  //classe nao declara com parenteses
+public class AlunoDeGraduacao{  //classe nao declara com parenteses
     public class interna; //excecao
     private String nome; //variavel de instancia, variavel de referencia, guarda um ponteiro que aponta para a classe String, inicializa com NULL
     private int matricula; //variavel de instancia(declarada dentro do corpo da classe) variavel primitiva, guarda um valor
@@ -23,8 +23,20 @@ public class ExampleForTest{  //classe nao declara com parenteses
 //objetos ficam na heap, eles que tem a informacao das variaveis na memoria
 //java nao tem lixo de memoria
 
-class Lorem{
+// class Lorem{
 
-}
+// }
 //cada classe tera um arquivo fonte, nao ha mais de uma por arquivo
 //pilha de execucao stack
+AlunoDeGraduacao[] pessoas = new AlunoDeGraduacao[x];
+AlunoDeGraduacao alunoStephano = new AlunoDeGraduacao(); //objeto criado com uma variavel apontando para o mesmo, usando o new
+int[] numeros = new int[5]; //todo vetor e um objeto, ja esta totalmente inicializado, vetor primitivo
+for (int i=0;i<numeros.length;i++){
+    System.out.printIn(numeros[i]);
+}
+for (int i=0;i<numeros.length;i++){
+    numeros[i]=10;
+}
+String pessoa1 = "Bruno";
+String pessoa2 = "Bruno";
+System.out.printIn(pessoa1.equals(pessoa2));
