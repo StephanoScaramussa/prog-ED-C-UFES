6f8db40380504b3eee259a536bea974e15b7ea1c
Stephano Dorigo Scaramussa <stephano.scaramussa>
2025-12-10 17:33:27 -0300

    ch

--- prog2/CollectionsV1/ComparaPessoaNome.java
+++ prog2/CollectionsV1/ComparaPessoaNome.java
@@ -3,7 +3,7 @@ import java.util.Comparator;
 public class ComparaPessoaNome implements Comparator<Pessoa>{
 
     @Override
-    public int compare(Pessoa p1, Pessoa p2){
+    public int compareTo(Pessoa p1, Pessoa p2){
         return p1.getNome().compareTo(p2.getNome());
     }
 }
