2d3df15efc987cca9d842b4867d1b055ea477ab0
Stephano <98617499+StephanoScaramussa@users.noreply.github.com>
2026-01-28 20:39:24 +0000

    INNSTALL JAVA

---
+++ prog2/JFRAMES001/TestaJFrame.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+public class TestaJFrame{
+    public static void main(String[] args){
+        JFrame quadro = new JFrame("Minha Aplicacao");
+        quadro.setVisible(true);
+        quadro.setSize(300, 300);
+        quadro.setLocationRelativeTo(null);
+        quadro.setVisible(true);
+        quadro.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Scanner sc = new Scanner(System.in);
+
+        // String titulo = sc.nextLine();
+
+        JButton botaoSomar = new JButton("Somar");
+        JTextField campoNumero1 = new JTextField();
+        quadro.add(botaoSomar);
+        quadro.add(campoNumero1);
+
+    }
+}
